28c4ed21ec5d52495a70da9a1340864c3da30494
pa1narendra <pavannarendra2002@gmail.com>
2024-08-28 10:50:32 +0530

    removed unnecessary things

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./index.css";
 
 const App = () => {
-  return <h1 className="text-emerald-300">Pavan Narendra</h1>;
+  return <h1 className="text-emerald-300 text-2xl">Pavan Narendra</h1>;
 };
 
 export default App;

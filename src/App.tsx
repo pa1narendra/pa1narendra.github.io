82851373c7264a3aaf2fbd662442e0f45fc57ea2
pa1narendra <pavannarendra2002@gmail.com>
2024-08-28 12:01:08 +0530

    modified navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import NavBar from "./Components/NavBar";
 import "./index.css";
 
 const App = () => {
-  return <h1 className="text-emerald-300 text-2xl">Pavan Narendra</h1>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,34 @@
+const NavBar = () => {
+  return (
+    <span className="flex justify-center my-5">
+      <div className=" bg-gray-400 w-1/2 rounded-xl flex flex-row justify-between items-center px-4">
+        <a className="text-3xl font-bold text-blue-50" href="/">
+          Pavan
+        </a>
+
+        <div className="flex py-4 gap-7">
+          <a
+            href="#projects"
+            className="text-white hover:bg-blue-400 hover:text-slate-900 px-3 py-2 rounded-md"
+          >
+            Projects
+          </a>
+          <a
+            href="#about"
+            className="text-white hover:bg-blue-400 hover:text-slate-900 px-3 py-2 rounded-md"
+          >
+            About
+          </a>
+          <a
+            href="#contact"
+            className="text-white hover:bg-blue-400 hover:text-slate-900 px-3 py-2 rounded-md"
+          >
+            Contact
+          </a>
+        </div>
+      </div>
+    </span>
+  );
+};
+
+export default NavBar;

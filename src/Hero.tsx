d4b3c657e4aced36d8a496915399c2e6e5efdd44
pa1narendra <pavannarendra2002@gmail.com>
2024-09-02 10:59:43 +0530

    hero

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,12 +1,9 @@
 const Hero = () => {
   return (
     <div id="#" className="pt-[70px]">
-      <h1 className="text-6xl mt-4 font-bold flex  justify-center">
-        Hello and welcome to my world
+      <h1 className="text-[150px] mt-4 font-bold flex  justify-center">
+        Pavan Narendra
       </h1>
-      <p className="text-center pt-12 text-2xl">
-        I am a Passionate developer from India.
-      </p>
     </div>
   );
 };

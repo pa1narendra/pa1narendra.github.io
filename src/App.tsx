2a5145101fa16ad2ce62e420a13123b2b09ddd62
pa1narendra <pavannarendra2002@gmail.com>
2024-09-02 18:27:58 +0530

    modified cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import NavBar from "./Components/NavBar";
 
-import Hero from "./Components/Hero";
-import "./index.css";
+
 
 const App = () => {
   return (
     <div className="">
       <NavBar />
-      <Hero />
+      
     </div>
   );
 };
--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,33 +1,41 @@
+import Hero from "./Hero";
+import "../App.css";
+
 const NavBar = () => {
   return (
-    <span className="sticky top-[30px] flex justify-center z-1">
-      <div className="bg-opacity-10 bg-black w-1/2 rounded-xl backdrop-blur-md flex flex-row justify-between items-center px-4">
-        <a className="text-3xl font-bold text-blue-700" href="/">
-          Pavan
-        </a>
+    <div>
+      <span className="sticky top-[30px] flex justify-center z-1">
+        <div className="bg-opacity-10 bg-black w-1/2 rounded-xl backdrop-blur-md flex flex-row justify-between items-center px-4">
+          <a className="text-3xl font-bold text-blue-700" href="/">
+            Pavan
+          </a>
 
-        <div className="flex py-4 gap-7">
-          <a
-            href="#projects"
-            className="text-blue-700 hover:bg-blue-400 hover:text-slate-900 px-3 py-2 rounded-md"
-          >
-            Projects
-          </a>
-          <a
-            href="#about"
-            className="text-blue-700 hover:bg-blue-400 hover:text-slate-900 px-3 py-2 rounded-md"
-          >
-            About
-          </a>
-          <a
-            href="#contact"
-            className="text-blue-700 hover:bg-blue-400 hover:text-slate-900 px-3 py-2 rounded-md"
-          >
-            Contact
-          </a>
+          <div className="flex py-4 gap-7">
+            <a
+              href="#projects"
+              className="text-blue-700 hover:bg-blue-400 hover:text-slate-900 px-3 py-2 rounded-md"
+            >
+              Projects
+            </a>
+            <a
+              href="#about"
+              className="text-blue-700 hover:bg-blue-400 hover:text-slate-900 px-3 py-2 rounded-md"
+            >
+              About
+            </a>
+            <a
+              href="#contact"
+              className="text-blue-700 hover:bg-blue-400 hover:text-slate-900 px-3 py-2 rounded-md"
+            >
+              Contact
+            </a>
+          </div>
         </div>
+      </span>
+      <div>
+        <Hero />
       </div>
-    </span>
+    </div>
   );
 };
 

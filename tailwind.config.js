3a6ea19aebe3a6e4222312f4304a70ebdabf26f5
pa1narendra <pavannarendra2002@gmail.com>
2024-09-13 18:29:31 +0530

    eotd

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        led: {
+          "0%": { fill: "currentColor", brightness: "1" },
+          "50%": { fill: "#a855f7", brightness: "500%" },
+          "100%": { fill: "currentColor", brightness: "1" },
+        },
+      },
+      animation: {
+        led: "led 100ms ease-in-out",
+      },
+    },
   },
   plugins: [],
 };

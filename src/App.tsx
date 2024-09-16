c7b4ecf9cf5e7b240e4b4a1283c1e0cbfc8ea3b1
pa1narendra <pavannarendra2002@gmail.com>
2024-09-16 11:59:14 +0530

    added ledboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import About from "./Components/About";
+import Hero from "./Components/Hero";
 import NavBar from "./Components/NavBar";
+import LEDBoard from "./Components/LedBoard";
 
 const App = () => {
   return (
     <div className="">
       <NavBar />
+      <LEDBoard word="PAVAN NARENDRA" />
+      <Hero />
+      <About />
     </div>
   );
 };

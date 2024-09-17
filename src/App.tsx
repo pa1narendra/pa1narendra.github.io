1933cef24ae908337af532c179138cc1f47d7e87
pa1narendra <pavannarendra2002@gmail.com>
2024-09-17 15:48:45 +0530

    removed demo file

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,15 @@ import LEDBoard from "./Components/LedBoard";
 import About from "./Components/About";
 import Hero from "./Components/Hero";
 import Projects from "./Components/Projects";
-import Demo from "./Components/Demo";
 
 const App = () => {
   return (
-    < div className="bg-zinc-800 text-gray-50">
+    <div className="bg-zinc-800 text-gray-50">
       <NavBar />
       <Hero />
       <About />
       <LEDBoard word="Pavan Narendra" />
       <Projects />
-        <Demo />
     </div>
   );
 };

e81f0cfc48e06501e1f5e6df7b01686293ec6249
pa1narendra <pavannarendra2002@gmail.com>
2024-09-17 17:21:14 +0530

    modified text aligns

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,7 +1,7 @@
 const About = () => {
   return (
-    <div id="about" className="h-screen">
-      <p className="text-center text-xl">
+    <div id="about" className="h-screen ">
+      <p className=" text-xl flex justify-center align-middle">
         I am a Software Engineer. I do a lot of experiments and explore a lot of
         fields. I am a part time coder.Few of my interests are chess, badminton,
         gossips, etc
--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,17 +1,15 @@
 const Projects = () => {
   return (
-    <div className="h-screen">
+    <div className="h-screen flex align-middle justify-center">
       <div id="projects">
         <h2>Here are few of my projects</h2>
         <img src="" alt="" />
-        <p className="">
           <p className="pro-description leading-5">
             This is my first project in my professional carrier and I had done
             it well. I worked on python and javascript in this project.
             JavaScript for frontend and python for Backend. This is a Frappe ERP
             NEXT project.
           </p>
-        </p>
       </div>
     </div>
   );

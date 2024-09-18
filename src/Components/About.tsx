691ad082ba2805648f564d42bf718fe9515c1006
pa1narendra <pavannarendra2002@gmail.com>
2024-09-18 18:31:38 +0530

    added about

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,8 +1,15 @@
 const About = () => {
   return (
     <div id="about" className="h-screen flex justify-center items-center">
-      <p className=" text-xl tracking-wider">
-        Hi there , I am <span className="bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">Pavan Narendra </span> a Software Engineer passionate about technology
+      <p className=" text-xl tracking-widest text-center w-1/2">
+        Hi there , I am{" "}
+        <span className="bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">
+          Pavan Narendra{" "}
+        </span>{" "}
+        a Software Engineer passionate about technology and innovation. Updating
+        my skill library with new tech books and creative articles. Open to
+        collaborate and discuss about any idea in our domain and build it and
+        bring it to live.
       </p>
     </div>
   );

1490c7be0f48872efa5f3238f9843f7c4d69b1e2
pa1narendra <pavannarendra2002@gmail.com>
2024-09-18 16:53:52 +0530

    updated about

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,7 +1,7 @@
 const About = () => {
   return (
     <div id="about" className="h-screen flex justify-center items-center">
-      <p className=" text-xl ">
+      <p className=" text-7xl ">
         I am a Software Engineer. I do a lot of experiments and explore a lot of
         fields. I am a part time coder.Few of my interests are chess, badminton,
         gossips, etc

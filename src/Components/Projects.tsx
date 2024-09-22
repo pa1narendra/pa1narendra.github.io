e9761b65767be2fd2576aa0363998d3727013f17
pa1narendra <pavannarendra2002@gmail.com>
2024-09-22 18:07:26 +0530

    sunday

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -4,10 +4,32 @@ const Projects = () => {
       id="projects"
       className="h-screen flex items-center justify-center flex-col"
     >
-      <h2 className="text-center">Here are few of my projects</h2>
+      <h2 className="text-center text-3xl mb-7">Here are few of my projects</h2>
       <div className="flex justify-center">
         <img src="" alt="" />
         <p className="pro-description  leading-5 text-2xl w-1/2">
+          Fresh Meals is an Idea generated from the confusion we faced when we
+          went to a new city and tried to have some food. There are several food
+          items and many restaurants to choose from. We thought it would be nice
+          to have an app that tells us what are the most famous dishes of that
+          area in that particular season , and which restaurants produce that
+          item.
+        </p>
+      </div>
+      <div className="flex justify-center">
+        <img src="" alt="" />
+        <p className="pro-description  leading-5 text-2xl w-1/2">
+          Fresh Meals is an Idea generated from the confusion we faced when we
+          went to a new city and tried to have some food. There are several food
+          items and many restaurants to choose from. We thought it would be nice
+          to have an app that tells us what are the most famous dishes of that
+          area in that particular season , and which restaurants produce that
+          item.
+        </p>
+      </div>
+      <div className="flex justify-center">
+        <img src="" alt="" />
+        <p className="pro-description text-slate-200 leading-5 text-2xl w-1/2">
           Fresh Meals is an Idea generated from the confusion we faced when we
           went to a new city and tried to have some food. There are several food
           items and many restaurants to choose from. We thought it would be nice

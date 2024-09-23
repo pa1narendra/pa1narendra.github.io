c87e79712293c90c86fbdb29c2eeefb580409369
pa1narendra <pavannarendra2002@gmail.com>
2024-09-23 15:39:26 +0530

    removed custom cursor

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,91 +1,12 @@
-import { useEffect, useState } from "react";
-import { motion, Variants } from "framer-motion";
 import "../App.css";
-
-interface MousePosition {
-  x: number;
-  y: number;
-}
-
-function Cursor() {
-  const [mousePosition, setMousePosition] = useState<MousePosition>({
-    x: 0,
-    y: 0,
-  });
-  const [cursorVariant, setCursorVariant] = useState<"default" | "text">(
-    "default"
-  );
-
-  useEffect(() => {
-    const mouseMove = (e: MouseEvent) => {
-      setMousePosition({
-        x: e.clientX,
-        y: e.clientY,
-      });
-    };
-
-    const textEnter = () => setCursorVariant("text");
-    const textLeave = () => setCursorVariant("default");
-
-    window.addEventListener("mousemove", mouseMove);
-    document.querySelectorAll("h1, h2").forEach((el) => {
-      el.addEventListener("mouseenter", textEnter);
-      el.addEventListener("mouseleave", textLeave);
-    });
-
-    // Make cursor normal on the navbar
-    const navBar = document.querySelector(".navbar");
-    const handleMouseEnter = () => setCursorVariant("default");
-    const handleMouseLeave = () => setCursorVariant("default");
-
-    if (navBar) {
-      navBar.addEventListener("mouseenter", handleMouseEnter);
-      navBar.addEventListener("mouseleave", handleMouseLeave);
-    }
-    return () => {
-      window.removeEventListener("mousemove", mouseMove);
-      document.querySelectorAll("h1, h2").forEach((el) => {
-        el.removeEventListener("mouseenter", textEnter);
-        el.removeEventListener("mouseleave", textLeave);
-      });
-
-      if (navBar) {
-        navBar.removeEventListener("mouseenter", textLeave);
-        navBar.removeEventListener("mouseleave", textLeave);
-      }
-    };
-  }, []);
-
-  const variants: Variants = {
-    default: {
-      x: mousePosition.x - 30,
-      y: mousePosition.y - 30,
-    },
-    text: {
-      height: 150,
-      width: 150,
-      x: mousePosition.x - 75,
-      y: mousePosition.y - 75,
-      backgroundColor: "#eee",
-      mixBlendMode: "soft-light",
-    },
-  };
-
+const Hero = () => {
   return (
-    <div className="App h-screen ">
-      <h1
-        className="title bg-gradient-to-r from-green-500 via-blue-500 to-purple-500 bg-clip-text text-transparent font-bold animate-gradient"
-        style={{ fontFamily: "Playpen Sans" }}
-      >
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="title text-center" style={{ fontFamily: "Playpen Sans" }}>
         Pavan Narendra
       </h1>
-      <motion.div
-        className="cursor"
-        variants={variants}
-        animate={cursorVariant}
-      />
     </div>
   );
-}
+};
 
-export default Cursor;
+export default Hero;

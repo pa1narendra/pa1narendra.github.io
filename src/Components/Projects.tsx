80e4da05257c67f92c441f31aa481e16eb8f808d
pa1narendra <pavannarendra2002@gmail.com>
2024-12-06 18:03:35 +0530

    💄 style: update project descriptions in Projects.tsx

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -2,13 +2,13 @@ const Projects = () => {
   return (
     <div
       id="projects"
-      className="h-screen flex items-center justify-center flex-col"
+      className="h-screen "
     >
       <h2 className="text-center text-3xl mb-7">Here are few of my projects</h2>
-      <div className="w-7/8 flex flex-col gap-4">
+      <div className="w-7/8 grid justify-center items-center grid-cols-2">
         <div className="flex justify-center p-10 gap-6">
           <img className="w-1/5 h-[200px]" src="" alt="" />
-          <p className=" text-2xl flex items-center w-1/3 li">
+          <p className=" text-xl w-1/3">
             Fresh Meals is an Idea generated from the confusion we faced when we
             went to a new city and tried to have some food. There are several
             food items and many restaurants to choose from. We thought it would
@@ -19,24 +19,20 @@ const Projects = () => {
         </div>
         <div className="flex justify-center p-10 gap-6">
           <img className="w-1/5 h-[200px]" src="" alt="" />
-          <p className="pro-description text-2xl w-1/3">
-            Fresh Meals is an Idea generated from the confusion we faced when we
-            went to a new city and tried to have some food. There are several
-            food items and many restaurants to choose from. We thought it would
-            be nice to have an app that tells us what are the most famous dishes
-            of that area in that particular season , and which restaurants
-            produce that item.
+          <p className="pro-description text-xl w-1/3">
+            Smart Grow is an IoT based project. This project helps farmers to
+            monitor their crops and to take necessary actions in order to
+            increase their yield. This project is based on the concept of
+            Internet of Things (IoT).
           </p>
         </div>
         <div className="flex justify-center p-10 gap-6">
           <img className="w-1/5 h-[200px]" src="" alt="" />
-          <p className="pro-description text-2xl w-1/3">
-            Fresh Meals is an Idea generated from the confusion we faced when we
-            went to a new city and tried to have some food. There are several
-            food items and many restaurants to choose from. We thought it would
-            be nice to have an app that tells us what are the most famous dishes
-            of that area in that particular season , and which restaurants
-            produce that item.
+          <p className="pro-description text-xl w-1/3">
+            CSE Site is a website that provides information about Computer Science
+            and Engineering Department of KITS Guntur. This website is built
+            using Traditional HTML, CSS and JS.It has all details about the events, 
+            faculty members, and other important information.
           </p>
         </div>
       </div>

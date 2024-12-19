002cf264a0badc06cdea43aa197585cf846225a6
pa1narendra <pavannarendra2002@gmail.com>
2024-12-19 17:11:46 +0530

    🚑 fix: Update PreLoader size and border color in PreLoader.tsx

--- src/Components/PreLoader.tsx
+++ src/Components/PreLoader.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Preloader: React.FC = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-75 z-50">
-      <div className="w-16 h-16 border-4 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
+      <div className="w-26 h-26 border-4 border-t-4 border-black-600 border-solid rounded-full animate-spin"></div>
     </div>
   );
 };
